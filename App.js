ea082abdc77a91b96757eb9869bc1e92fff1851d
newmade01 <newmade01@naver.com>
2020-12-07 13:23:52 +0900

    Screen

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 //Launcher Activity
-//main
 
-//import libra
+//import 라이브러리
 import { StatusBar } from 'expo-status-bar';
 import *as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';//navigation 활성화
+import {createStackNavigator} from '@react-navigation/stack';
+
+import main from './screens/main';
+
+
+const Stack=createStackNavigator();
+
+
 //export
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return (//화면 반환
+    //Navigationxm틀//const stack 변수명에따라 변경됨 . navigator
+    <NavigationContainer> 
+      <Stack.Navigator
+        screenOptions={{
+          headerShown:false,//상단바 없앰
+        }}
+      >
+      
+      <Stack.Screen name="첫화면" component={main}/>
+      
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
 //CSS
 const styles = StyleSheet.create({
   container: {
---
+++ screens/main.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class main extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            
+
+        )
+    }
+
+}
